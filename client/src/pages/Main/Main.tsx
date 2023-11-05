c7cdad317b282094601a13f2a835f7c8a71ff9f9
Вова <v.grishchenko@onpoint.ru>
2023-11-05 21:52:22 +0300

    finish adBanner

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,22 +1,26 @@
 import PopularCategoties from "@/modules/PopularCategoties/PopularCategoties";
 import Slider from "@/components/Slider/Slider";
+import { adBanners } from "./data";
 
-import styles from "./Main.module.scss";
+import AdBanner from "@/modules/AdBanner/AdBanner";
 
+const sliderImages = [
+  "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week43/27_10/bnr-w44s3.webp",
+  "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week44/31_10/bnr-w44s7.webp",
+  "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week44/31_10/bnr-w44s6.webp",
+  "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week44/31_10/bnr-w44s9.webp",
+];
+const sliderItems = sliderImages.map((image, index) => (
+  <img key={index} src={image} alt="banner" style={{ width: "100%" }} />
+));
+
+const [firstBanner] = [...adBanners];
 export default function Main() {
-  const sliderImages = [
-    "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week11/13_03/bnr-w11sPromo.webp",
-    "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week10/09_03/bnr-w11s3.webp",
-    "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week11/13_03/bnr-w11s4.webp",
-  ];
-  const sliderItems = sliderImages.map((image, index) => (
-    <img key={index} src={image} alt="banner" style={{ width: "100%" }} />
-  ));
-
   return (
     <>
       <Slider sliderItems={sliderItems} />
       <PopularCategoties />
+      <AdBanner {...firstBanner} />
     </>
   );
 }
---
+++ client/src/pages/Main/data.ts
@@ -0,0 +1,11 @@
+export const adBanners = [
+  {
+    srcDesc:
+      "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week43/27_10/slot2-dv-geo.webp",
+    srcMob:
+      "https://cdn.sportmaster.ru/upload/content/cmsgate/ru_sm/smprod/dip_content/2023/sm30/week43/27_10/slot2-mv-geo.webp",
+    bannerTitle: "Новые бренды со скидками",
+    bannerText: "aTech nutrition, RLINE SPORT NUTRITION, MUTANT и другие",
+    buttonText: "Купить",
+  },
+];
